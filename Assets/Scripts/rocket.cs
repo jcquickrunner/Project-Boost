569bdfb3ee107d3084a198c7d18ba1ed97e7549f
Juan <juan@seasonmint.com>
2020-01-23 20:41:53 -0500

    audio and movement

--- Assets/Scripts/rocket.cs
+++ Assets/Scripts/rocket.cs
@@ -1,18 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
+    AudioSource thrust;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+         thrust = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessInput();
+    }
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("space pressed");
+            rigidBody.AddRelativeForce(Vector3.up);//relative to postition
+            if (!thrust.isPlaying)
+            {
+                thrust.Play();
+            } else
+            {
+                thrust.Stop();
+                
+            }
+            
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("rotating left");
+            transform.Rotate(Vector3.forward);
+           
+        } else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Right");
+            transform.Rotate(-Vector3.forward);//- vector forward would work here as well
+
+        }
+
     }
 }
