1950594e61dccd26f29a873d45700e5790824cd4
Juan <juan@seasonmint.com>
2020-01-23 21:28:11 -0500

    add more variation in rotation

--- Assets/Scripts/rocket.cs
+++ Assets/Scripts/rocket.cs
@@ -7,6 +7,7 @@ public class rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
     AudioSource thrust;
+    [SerializeField] float rcsThrust = 100f;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,31 +25,52 @@ public class rocket : MonoBehaviour
     }
     private void ProcessInput()
     {
+        ThrustMovement();
+        RocketRotation();
+        ThrustSound();
+
+    }
+
+    private void ThrustMovement()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
             print("space pressed");
             rigidBody.AddRelativeForce(Vector3.up);//relative to postition
 
         }
+    }
+
+    private void RocketRotation()
+    {
+        rigidBody.freezeRotation = true;
+       
         if (Input.GetKey(KeyCode.A))
         {
             print("rotating left");
-            transform.Rotate(Vector3.forward);
+            float rotationSpeedThisFrame = rcsThrust * Time.deltaTime;
+            transform.Rotate(Vector3.forward * rotationSpeedThisFrame);
 
         }
         else if (Input.GetKey(KeyCode.D))
         {
+            float rotationSpeedThisFrame = rcsThrust * Time.deltaTime;
             print("Rotating Right");
-            transform.Rotate(-Vector3.forward);//- vector forward would work here as well
+            transform.Rotate(-Vector3.forward * rotationSpeedThisFrame);//- vector forward would work here as well
 
         }
+        rigidBody.freezeRotation = !true;
+    }
+
+    private void ThrustSound()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (!thrust.isPlaying)
             {
                 thrust.Play();
             }
-            
+
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
@@ -58,7 +80,6 @@ public class rocket : MonoBehaviour
             }
 
         }
-
     }
 }
      
