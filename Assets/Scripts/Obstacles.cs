23e6a7d1ee0c867fa680c49184c8186db72fb782
Juan <juan@seasonmint.com>
2020-01-28 19:42:46 -0500

    obstacles implemented

---
+++ Assets/Scripts/Obstacles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 0f);
+    [SerializeField] float period = 2f;
+    // Start is called before the first frame update
+    [Range (0,1)][SerializeField] float movementFactor;
+    Vector3 startingPos;
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period;// grows continually from zero
+        const float tau = Mathf.PI * 2; //about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau);
+        movementFactor = rawSinWave / 2f + 0.5f;
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
--- Assets/Scripts/rocket.cs
+++ Assets/Scripts/rocket.cs
@@ -10,8 +10,15 @@ public class rocket : MonoBehaviour
     Rigidbody rigidBody;
     AudioSource thrust;
     [SerializeField] float rcsThrust = 100f;
+    [SerializeField] AudioClip mainEngine;//stores audioclip, way to set audi in code
+    [SerializeField] AudioClip explosion;
+    [SerializeField] AudioClip transitionToMainMenu;
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem deathParticles;
     // Start is called before the first frame update
-    
+    enum State {  alive, dying, changing};
+    State state = State.alive;
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
@@ -24,10 +31,19 @@ public class rocket : MonoBehaviour
         switch (collision.gameObject.tag)
         {
             case "Enemy":
-                SceneManager.LoadScene(1);
+                state = State.dying;
+                thrust.Stop();
+                mainEngineParticles.Stop();
+                thrust.PlayOneShot(explosion);
+                deathParticles.Play();
+                Invoke("LoadLevel", 2f);//so invoke basically is a way to delay a function but have others continue running without stopping the program.
                 break;
             case "Finish":
-                SceneManager.LoadScene(2);
+                state = State.changing;
+                thrust.Stop();
+                successParticles.Play();
+                thrust.PlayOneShot(transitionToMainMenu);
+                Invoke("LoadMenu", 2f);
 
                 break;
             default:
@@ -35,6 +51,16 @@ public class rocket : MonoBehaviour
 
 
         }
+    }
+
+    private void LoadMenu()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    private void LoadLevel()
+    {
+        SceneManager.LoadScene(1);
     }
 
     // Update is called once per frame
@@ -58,53 +84,77 @@ public class rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        ThrustMovement();
-        RocketRotation();
-        ThrustSound();
+        if (state == State.alive)
+        {
+            ThrustMovement();
+            RocketRotation();
+            ThrustSound();
+            
+
+        }
+       /* if (state != State.alive)
+        {
+            thrust.Stop();
+        } */
 
     }
 
     private void ThrustMovement()
-    {
-        if (Input.GetKey(KeyCode.Space))
+    { if (state == State.alive)
         {
-            print("space pressed");
-            rigidBody.AddRelativeForce(Vector3.up);//relative to postition
+            if (Input.GetKey(KeyCode.Space))
+            {
+                print("space pressed");
 
+                rigidBody.AddRelativeForce(Vector3.up);//relative to postition
+                mainEngineParticles.Play();
+            }
+            else mainEngineParticles.Stop();
+            
         }
+        
+
     }
 
     private void RocketRotation()
     {
-        rigidBody.freezeRotation = true;
-        
-        if (Input.GetKey(KeyCode.A))
+        if (state == State.alive)
         {
-            print("rotating left");
-            float rotationSpeedThisFrame = rcsThrust * Time.deltaTime;
-            transform.Rotate(Vector3.forward * rotationSpeedThisFrame);
+            rigidBody.freezeRotation = true;
 
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            float rotationSpeedThisFrame = rcsThrust * Time.deltaTime;
-            print("Rotating Right");
-            transform.Rotate(-Vector3.forward * rotationSpeedThisFrame);//- vector forward would work here as well
+            if (Input.GetKey(KeyCode.A))
+            {
+                print("rotating left");
+                float rotationSpeedThisFrame = rcsThrust * Time.deltaTime;
+                transform.Rotate(Vector3.forward * rotationSpeedThisFrame);
 
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                float rotationSpeedThisFrame = rcsThrust * Time.deltaTime;
+                print("Rotating Right");
+                transform.Rotate(-Vector3.forward * rotationSpeedThisFrame);//- vector forward would work here as well
+
+            }
+            rigidBody.freezeRotation = !true;
         }
-        rigidBody.freezeRotation = !true;
+
     }
 
     private void ThrustSound()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (state == State.alive)
         {
-            if (!thrust.isPlaying)
+            if (Input.GetKey(KeyCode.Space))
             {
-                thrust.Play();
-            }
+                if (!thrust.isPlaying)
+                {
+                    thrust.PlayOneShot(mainEngine);
+                }
 
+            }
         }
+       
         if (Input.GetKeyUp(KeyCode.Space))// so when you stop pressing space the sound stops
         {
             if (thrust.isPlaying)
